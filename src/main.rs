0b5fc0fff211b098a9f6296cc42d45a2595329fd
Brett Chalupa <brettchalupa@gmail.com>
2022-09-28 21:10:57 -0400

    Initial basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::error::Error;
+
+/// The rwc utility displays the number of words in a file. Inspired by `wc`.
+#[derive(Parser)]
+struct Cli {
+    /// The path to the file to read
+    files: Vec<String>
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+
+    let mut summary:String = String::from("");
+    let mut total_word_count = 0;
+    let file_count = args.files.len();
+
+    for file in args.files {
+        let content = std::fs::read_to_string(&file)?;
+        let mut file_word_count = 0;
+
+        for line in content.lines() {
+            if line.trim().is_empty() {
+                continue
+            }
+            let count = line.split(' ').count();
+            total_word_count += count;
+            file_word_count += count;
+        }
+
+        if file_count > 1 {
+            summary = format!("{}{}: {}\n", summary, file, file_word_count);
+        }
+    }
+
+    if file_count > 1 {
+        summary = format!("{}{}: {}", summary, "total", total_word_count);
+    } else {
+        summary = format!("{}", total_word_count);
+    }
+
+    println!("{}", summary);
+    Ok(())
 }
---
+++ tests/cli.rs
@@ -0,0 +1,60 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn single_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("rwc")?;
+
+    cmd.arg("tests/fixtures/haiku.txt");
+    cmd.assert().success().stdout(String::from("7\n"));
+
+    Ok(())
+}
+
+#[test]
+fn medium_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("rwc")?;
+
+    cmd.arg("tests/fixtures/medium.txt");
+    cmd.assert().success().stdout(String::from("8\n"));
+
+    Ok(())
+}
+
+#[test]
+fn long_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("rwc")?;
+
+    cmd.arg("tests/fixtures/long.txt");
+    cmd.assert().success().stdout(String::from("204\n"));
+
+    Ok(())
+}
+
+#[test]
+fn multiple_files() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("rwc")?;
+
+    cmd.arg("tests/fixtures/haiku.txt")
+        .arg("tests/fixtures/medium.txt")
+        .arg("tests/fixtures/long.txt");
+    cmd.assert().success().stdout(String::from("tests/fixtures/haiku.txt: 7\ntests/fixtures/medium.txt: 8\ntests/fixtures/long.txt: 204\ntotal: 219\n"));
+
+    Ok(())
+}
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("rwc")?;
+
+    cmd.arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
+
+// TODO: directory error
+// wc: tests/fixtures/: read: Is a directory
